232ab21cc707a6c3fb241eb81a930c34acc3f7be
Stanislav <skonashuk@list.ru>
2021-12-13 00:30:32 +0300

    Add Footer component in App component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css'
 import Header from './Header/Header'
+import Content from './Content/Content'
+import Footer from './Footer/Footer'
 
 function App({ state }) {
   return (
     <div className="App">
       <Header navItems={state.navItems} />
+      <Content
+        projects={state.projects}
+        atticProjects={state.atticProjects}
+        projectsFooterItems={state.projectsFooterItems}
+      />
+      <Footer footerLinks={state.footerLinks} />
     </div>
   )
 }
