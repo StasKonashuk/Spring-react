b0f1a3edacfc9c6e470d72297980bde61011edc8
Stanislav <skonashuk@list.ru>
2021-12-13 14:57:02 +0300

    Add key in Project component props

--- src/Content/Projects.js
+++ src/Content/Projects.js
@@ -1,16 +1,16 @@
-import React from 'react'
-import style from './Content.module.css'
-import Project from './Project'
+import React, { useEffect } from 'react';
+import style from './Content.module.css';
+import Project from './Project';
 
 const Projects = ({ projects }) => {
-  const project = [...projects].map(p => (
-    <Project imageUrl={p.imageUrl} title={p.title} text={p.text} />
-  ))
+  const project = projects.map(p => (
+    <Project imageUrl={p.imageUrl} title={p.title} text={p.text} key={p.id} />
+  ));
   return (
     <div className={style.projContent}>
       <div className={style.projectBoxContainer}>{project}</div>
     </div>
-  )
-}
+  );
+};
 
-export default Projects
+export default Projects;
