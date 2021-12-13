baa851a84f355b117fc53c461453d5e9ec8ff9c7
Stanislav <skonashuk@list.ru>
2021-12-13 14:43:24 +0300

    Fix AtticProjects component

--- src/Content/AtticProjects.js
+++ src/Content/AtticProjects.js
@@ -1,17 +1,18 @@
-import React from 'react'
-import AtticProject from './AtticProject'
-import style from './Content.module.css'
+import React from 'react';
+import AtticProject from './AtticProject';
+import style from './Content.module.css';
+import data from '../data/data';
 
-const AtticProjects = ({ atticProjects }) => {
-  const atticProject = [...atticProjects].map(p => (
-    <AtticProject title={p.title} text={p.text} />
-  ))
+const AtticProjects = () => {
+  const atticProject = data.atticProjects.map(p => (
+    <AtticProject title={p.title} text={p.text} key={p.id} />
+  ));
   return (
     <div className={style.atticProjContent}>
       <h2 className={style.atticProjTitle}>Projects in the Attic</h2>
       <div className={style.atticProjBoxContainer}>{atticProject}</div>
     </div>
-  )
-}
+  );
+};
 
-export default AtticProjects
+export default AtticProjects;
