a999f8f54a656dda80baef860139b7c315f2f0aa
Stanislav <skonashuk@list.ru>
2021-12-17 19:47:50 +0300

    Add projects selectors

---
+++ src/redux/projects-selectors.js
@@ -0,0 +1,9 @@
+export const getProjects = state => {
+  return state.projects.projects;
+};
+export const getFooterProjectsItem = state => {
+  return state.projects.projectsFooterItems;
+};
+export const getAtticProjects = state => {
+  return state.projects.atticProjects;
+};
