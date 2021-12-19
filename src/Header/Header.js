b212fcedcfe2833bb35b6e211d7f8ad48b44babf
Stanislav <skonashuk@list.ru>
2021-12-19 23:31:09 +0300

    Add logout button to Header component

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Logo from './Logo';
 import style from './Header.module.css';
 import HeaderItems from './HeaderItems';
+import LogOutLink from './LogOutLink';
 
 const Header = () => {
   return (
     <header className={style.nav}>
       <div className={style.navInnerContainer}>
         <Logo />
+        <LogOutLink />
         <HeaderItems />
       </div>
     </header>
