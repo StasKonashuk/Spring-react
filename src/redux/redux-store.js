5cc94d53592dc7ec49fdaf2047848bfbbf7f8133
Stanislav <skonashuk@list.ru>
2021-12-19 23:27:46 +0300

    Add authReducer to redux store

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -1,9 +1,13 @@
-import { combineReducers, createStore } from 'redux';
-import projectsReducer from './projects-reducer';
+import { combineReducers, createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import projectsReducer from './projects/projects-reducer';
+import authReducer from './auth/auth-reducer';
 
 const reducers = combineReducers({
-  projects: projectsReducer
+  projects: projectsReducer,
+  auth: authReducer
 });
-const store = createStore(reducers);
+
+const store = createStore(reducers, applyMiddleware(thunk));
 
 export default store;
