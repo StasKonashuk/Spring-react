b3d9a279fc472761c980ed3990009db33bcc6e8e
Stanislav <skonashuk@list.ru>
2021-12-24 16:42:44 +0300

    Create thunk to login

--- src/redux/auth/auth-thunks-creators.js
+++ src/redux/auth/auth-thunks-creators.js
@@ -1,17 +1,14 @@
 import { actions } from './auth-actions-creators';
-
-const checkCredentials = params => {
-  if (params.userName.trim() !== 'admin' || params.password.trim() !== '1234') {
-    return false;
-  }
-  return true;
-};
+import { authAPI } from '../../api/api';
 
 export const logIn = params => {
-  return dispatch => {
-    if (checkCredentials(params)) {
-      dispatch(actions.logIn(params));
-    } else {
+  return async dispatch => {
+    try {
+      const data = await authAPI.login(params.userName, params.password);
+      if (data.status === 200) {
+        dispatch(actions.logIn(params));
+      }
+    } catch {
       dispatch(actions.logInFail('Invalid username or password'));
     }
   };
