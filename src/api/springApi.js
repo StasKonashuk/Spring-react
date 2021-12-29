8f5413b98e5f27b58dbbbe646619cad2fcf85bca
Stanislav <skonashuk@list.ru>
2021-12-29 12:10:52 +0300

    Change baseurl for redux toolkit query

--- src/api/springApi.js
+++ src/api/springApi.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const springApi = createApi({
   reducerPath: 'springApi',
   baseQuery: fetchBaseQuery({
-    baseUrl: 'http://localhost:5000/'
+    baseUrl: 'https://spring-react-server.herokuapp.com'
   }),
   endpoints: build => ({
     getProjects: build.query({
