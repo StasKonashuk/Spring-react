869f064e3e452f2cd8f13070b9b2a8c63f020cf1
Stanislav <skonashuk@list.ru>
2021-12-29 05:11:29 +0300

    Create redux toolkit request

---
+++ src/api/springApi.js
@@ -0,0 +1,22 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const springApi = createApi({
+  reducerPath: 'springApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'http://localhost:5000/'
+  }),
+  endpoints: build => ({
+    getProjects: build.query({
+      query: () => 'projects'
+    }),
+    auth: build.mutation({
+      query: ({ userName, password }) => ({
+        url: 'login',
+        method: 'POST',
+        body: { userName, password }
+      })
+    })
+  })
+});
+
+export const { useGetProjectsQuery, useAuthMutation } = springApi;
