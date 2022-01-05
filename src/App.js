5242e99749cf1a56b80dff9478cd010b45e99954
Stanislav <skonashuk@list.ru>
2022-01-05 15:11:22 +0300

    Add auth check and signup route

--- src/App.js
+++ src/App.js
@@ -5,7 +5,10 @@ import Footer from './Footer/Footer';
 import { Route, Routes, Navigate } from 'react-router-dom';
 import LoginForm from './Login/LoginForm';
 import NotFound from './common/NotFound';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import SignUpForm from './SignUp/SignUpForm';
+import { useEffect } from 'react';
+import { checkAuth } from './redux/auth/auth-thunks-creators';
 
 function App() {
   return (
@@ -13,12 +16,19 @@ function App() {
       <Route path="/spring-react" element={<Navigate to="/login" />} />
       <Route path="/" element={<Main />} />
       <Route path="/login" element={<LoginForm />} />
+      <Route path="/signup" element={<SignUpForm />} />
       <Route path="/*" element={<NotFound />} />
     </Routes>
   );
 }
 
 const Main = () => {
+  const dispatch = useDispatch();
+
+  useEffect(async () => {
+    if (localStorage.getItem('access')) dispatch(checkAuth());
+  }, []);
+
   const isAuth = useSelector(state => state.auth.isAuth);
 
   if (!isAuth) {
