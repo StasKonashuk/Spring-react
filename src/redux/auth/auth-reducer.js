cbf1f0b727b7e0a85c0ed672dbb9fd61d52bfb1e
Stanislav <skonashuk@list.ru>
2022-01-05 15:04:26 +0300

    Fix auth reducer

--- src/redux/auth/auth-reducer.js
+++ src/redux/auth/auth-reducer.js
@@ -5,27 +5,59 @@ const authSlice = createSlice({
   initialState: {
     user: null,
     errorMsg: '',
-    isAuth: false
+    isAuth: false,
+    access: localStorage.getItem('access'),
+    refresh: localStorage.getItem('refresh'),
+    message: '',
+    signUpErrors: {}
   },
   reducers: {
     logInSuccess(state, action) {
-      state.user = {
-        name: action.payload
-      };
+      localStorage.setItem('access', action.payload.access);
+      localStorage.setItem('refresh', action.payload.refresh);
       state.errorMsg = '';
       state.isAuth = true;
+      state.access = action.payload.access;
+      state.refresh = action.payload.refresh;
     },
     logOut(state) {
-      state.user = null;
-      state.errorMsg = '';
+      localStorage.removeItem('access');
+      localStorage.removeItem('refresh');
       state.isAuth = false;
+      state.access = null;
+      state.refresh = null;
     },
     logInFail(state, action) {
+      state.isAuth = false;
       state.errorMsg = action.payload;
+      state.access = null;
+      state.refresh = null;
+    },
+    signUp(state, action) {
+      state.message = action.payload;
+    },
+    signUpFail(state, action) {
+      state.signUpErrors = action.payload.signUpErrors;
+    },
+    signUpRefresh(state) {
+      state.message = null;
+      state.signUpErrors = {};
+    },
+    setAuth(state, action) {
+      state.access = action.payload;
+      state.isAuth = true;
     }
   }
 });
 
-export const { logInSuccess, logOut, logInFail } = authSlice.actions;
+export const {
+  logInSuccess,
+  logOut,
+  logInFail,
+  signUp,
+  signUpFail,
+  signUpRefresh,
+  setAuth
+} = authSlice.actions;
 
 export default authSlice.reducer;
