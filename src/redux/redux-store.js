3c10782c6a2ea4193a5d9037b247393dbdab206f
Stanislav <skonashuk@list.ru>
2022-01-09 20:28:15 +0300

    Add RTQ to redux store

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -1,13 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { setupListeners } from '@reduxjs/toolkit/dist/query';
+import { springApi } from '../api/springApi';
 import auth from './auth/auth-reducer';
 import projects from './projects/projects-reducer';
 
 export const store = configureStore({
   reducer: {
     auth,
-    projects
-  }
+    projects,
+    [springApi.reducerPath]: springApi.reducer
+  },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware().concat(springApi.middleware)
 });
 
 setupListeners(store.dispatch);
