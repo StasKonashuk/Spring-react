ebcf301cd94a0513bb4b4005f47dd5e2b7b9a467
Stanislav <skonashuk@list.ru>
2022-01-10 12:00:53 +0300

    Fix authApi

--- src/api/authApi.js
+++ src/api/authApi.js
@@ -1,33 +1,5 @@
 import { springApi } from './springApi';
 
-/* export const authAPI = {
-  login({ userName, password }) {
-    return springApi
-      .post('/api/login', { userName, password })
-      .then(res => res)
-      .catch(error => error.response);
-  },
-  signUp({ userName, password, repeatPassword, firstName, lastName, age }) {
-    return springApi
-      .post('/api/registration', {
-        userName,
-        password,
-        repeatPassword,
-        firstName,
-        lastName,
-        age
-      })
-      .then(res => res)
-      .catch(error => error.response);
-  },
-  logout() {
-    return springApi.delete('/api/refresh-token');
-  },
-  refresh() {
-    return springApi.get('/api/refresh-token').then(res => res.data);
-  }
-}; */
-
 const authApi = springApi.injectEndpoints({
   endpoints: builder => ({
     logIn: builder.mutation({
